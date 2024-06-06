dce8cd22188c41aceb4bf07158dd067de19b488c
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-06 19:07:43 +0200

    cange element in the condition part

--- myscript.js
+++ myscript.js
@@ -3,14 +3,17 @@
 const contenitore = document.querySelector("#container");
 //Write a cycle of numbers that goes from 1 to 100
 for (let i = 1; i <= 100; i++) {
-    const element = `<div class="box">${i}</div>`;
+    let element = `<div class="box">${i}</div>`;
     //Adding first condition
     if (i % 3 === 0){//the number is a multiple of 3, then print “Fizz” in place of the number.
         console.log("Fizz");
+        element =  `<div class="box m_3">${'Fizz'}</div>`;
     }else if (i % 5 === 0){//the number is a multiple of 5, then print “Buzz” in place of the number.
         console.log("Buzz");
+        element =  `<div class="box m_5">${'Buzz'}</div>`;
     } if (i % 3 === 0 && i%5 === 0){//the number is a multiple of 3 and 5, then print “FizzBuzz” in place of the number.
         console.log("FizzBuzz");
+        element =  `<div class="box m_3_5">${'FizzBuzz'}</div>`;
     }else{
         console.log(i);
     }
